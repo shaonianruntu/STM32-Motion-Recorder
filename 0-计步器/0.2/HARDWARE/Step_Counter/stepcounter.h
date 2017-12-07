720efb097386bf68291b0a31c572689428f32117
少年闰土 <514574496@qq.com>
2017-12-07 16:34:40 +0800

    Add files via upload

---
+++ 0-计步器/0.2/HARDWARE/Step_Counter/stepcounter.h
@@ -0,0 +1,16 @@
+#ifndef __STEPCOUNTER_H
+#define __STEPCOUNTER_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "mpu6050.h"
+#include "inv_mpu.h"
+#include "inv_mpu_dmp_motion_driver.h" 
+#include "lcd.h"
+#include "delay.h"
+#include "led.h"
+#include "sys.h"
+
+void stepcounter_main(void);
+
+#endif /* __STEPCOUNTER_H */
