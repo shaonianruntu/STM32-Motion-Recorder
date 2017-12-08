5ef1488b793c7dda2703cb64534a6ee4dbe51965
少年闰土 <514574496@qq.com>
2017-12-08 10:59:52 +0800

    Add files via upload

---
+++ 1-计步器+GUI/1.2/GUI/Windows/Codes.c
@@ -0,0 +1,192 @@
+/*
+******************************************************************
+**                      uCGUIBuilder                            **
+**                  Version:   4.0.0.0                          **
+**                     2012 / 04                               **
+**                   CpoyRight to: wyl                          **
+**              Email:ucguibuilder@163.com                        **
+**          This text was Created by uCGUIBuilder               **
+******************************************************************/
+
+#include <stddef.h>
+#include "GUI.h"
+#include "DIALOG.h"
+
+#include "WM.h"
+#include "BUTTON.h"
+#include "CHECKBOX.h"
+#include "DROPDOWN.h"
+#include "EDIT.h"
+#include "FRAMEWIN.h"
+#include "LISTBOX.h"
+#include "MULTIEDIT.h"
+#include "RADIO.h"
+#include "SLIDER.h"
+#include "TEXT.h"
+#include "PROGBAR.h"
+#include "SCROLLBAR.h"
+#include "LISTVIEW.h"
+
+
+
+
+
+//EventsFunctionList
+//EndofEventsFunctionList
+
+
+/*********************************************************************
+*
+*       static data
+*
+**********************************************************************
+*/
+
+
+
+/*********************************************************************
+*
+*       Dialog resource
+*
+* This table conatins the info required to create the dialog.
+* It has been created by ucGUIbuilder.
+*/
+
+static const GUI_WIDGET_CREATE_INFO _aDialogCreate[] = {
+    { FRAMEWIN_CreateIndirect,  "Caption",           0,                       0,  0,  240,320,FRAMEWIN_CF_MOVEABLE,0},
+    { BUTTON_CreateIndirect,    "Choose",            GUI_ID_BUTTON0,          18, 42, 203,64, 0,0},
+    { BUTTON_CreateIndirect,    "Sure",              GUI_ID_BUTTON1,          18, 137,69, 34, 0,0},
+    { BUTTON_CreateIndirect,    "Canel",             GUI_ID_BUTTON1,          152,137,69, 34, 0,0}
+};
+
+
+
+/*****************************************************************
+**      FunctionName:void PaintDialog(WM_MESSAGE * pMsg)
+**      Function: to initialize the Dialog items
+**                                                      
+**      call this function in _cbCallback --> WM_PAINT
+*****************************************************************/
+
+void PaintDialog(WM_MESSAGE * pMsg)
+{
+    WM_HWIN hWin = pMsg->hWin;
+
+}
+
+
+
+/*****************************************************************
+**      FunctionName:void InitDialog(WM_MESSAGE * pMsg)
+**      Function: to initialize the Dialog items
+**                                                      
+**      call this function in _cbCallback --> WM_INIT_DIALOG
+*****************************************************************/
+
+void InitDialog(WM_MESSAGE * pMsg)
+{
+    WM_HWIN hWin = pMsg->hWin;
+    //
+    //FRAMEWIN
+    //
+    FRAMEWIN_SetFont(hWin,&GUI_Font16_ASCII);
+    FRAMEWIN_AddCloseButton(hWin, FRAMEWIN_BUTTON_RIGHT, 0);
+    FRAMEWIN_AddMaxButton(hWin, FRAMEWIN_BUTTON_RIGHT, 1);
+    FRAMEWIN_AddMinButton(hWin, FRAMEWIN_BUTTON_RIGHT, 2);
+    FRAMEWIN_SetTitleHeight(hWin,16);
+    //
+    //GUI_ID_BUTTON0
+    //
+    BUTTON_SetBkColor(WM_GetDialogItem(hWin,GUI_ID_BUTTON0),BUTTON_CI_UNPRESSED,0xffffc0);
+    BUTTON_SetBkColor(WM_GetDialogItem(hWin,GUI_ID_BUTTON0),BUTTON_CI_PRESSED,0x00d7ff);
+    BUTTON_SetFont(WM_GetDialogItem(hWin,GUI_ID_BUTTON0),&GUI_Font16_ASCII);
+    //
+    //GUI_ID_BUTTON1
+    //
+    BUTTON_SetFont(WM_GetDialogItem(hWin,GUI_ID_BUTTON1),&GUI_Font16_ASCII);
+    //
+    //GUI_ID_BUTTON1
+    //
+    BUTTON_SetFont(WM_GetDialogItem(hWin,GUI_ID_BUTTON1),&GUI_Font16_ASCII);
+
+}
+
+
+
+
+/*********************************************************************
+*
+*       Dialog callback routine
+*/
+static void _cbCallback(WM_MESSAGE * pMsg) 
+{
+    int NCode, Id;
+    WM_HWIN hWin = pMsg->hWin;
+    switch (pMsg->MsgId) 
+    {
+        case WM_PAINT:
+            PaintDialog(pMsg);
+            break;
+        case WM_INIT_DIALOG:
+            InitDialog(pMsg);
+            break;
+        case WM_KEY:
+            switch (((WM_KEY_INFO*)(pMsg->Data.p))->Key) 
+            {
+                case GUI_KEY_ESCAPE:
+                    GUI_EndDialog(hWin, 1);
+                    break;
+                case GUI_KEY_ENTER:
+                    GUI_EndDialog(hWin, 0);
+                    break;
+            }
+            break;
+        case WM_NOTIFY_PARENT:
+            Id = WM_GetId(pMsg->hWinSrc); 
+            NCode = pMsg->Data.v;        
+            switch (Id) 
+            {
+                case GUI_ID_OK:
+                    if(NCode==WM_NOTIFICATION_RELEASED)
+                        GUI_EndDialog(hWin, 0);
+                    break;
+                case GUI_ID_CANCEL:
+                    if(NCode==WM_NOTIFICATION_RELEASED)
+                        GUI_EndDialog(hWin, 0);
+                    break;
+
+            }
+            break;
+        default:
+            WM_DefaultProc(pMsg);
+    }
+}
+
+
+/*********************************************************************
+*
+*       MainTask
+*
+**********************************************************************
+*/
+void Task_One(void) 
+{ 
+//  GUI_Init();
+//  WM_SetDesktopColor(GUI_WHITE);      /* Automacally update desktop window */
+    WM_SetCreateFlags(WM_CF_MEMDEV);  /* Use memory devices on all windows to avoid flicker */
+    //PROGBAR_SetDefaultSkin(PROGBAR_SKIN_FLEX);
+    //FRAMEWIN_SetDefaultSkin(FRAMEWIN_SKIN_FLEX);
+    //PROGBAR_SetDefaultSkin(PROGBAR_SKIN_FLEX);
+    //BUTTON_SetDefaultSkin(BUTTON_SKIN_FLEX);
+    //CHECKBOX_SetDefaultSkin(CHECKBOX_SKIN_FLEX);
+    //DROPDOWN_SetDefaultSkin(DROPDOWN_SKIN_FLEX);
+    //SCROLLBAR_SetDefaultSkin(SCROLLBAR_SKIN_FLEX);
+    //SLIDER_SetDefaultSkin(SLIDER_SKIN_FLEX);
+    //HEADER_SetDefaultSkin(HEADER_SKIN_FLEX);
+    //RADIO_SetDefaultSkin(RADIO_SKIN_FLEX);
+	while(1)
+	{
+        GUI_ExecDialogBox(_aDialogCreate, GUI_COUNTOF(_aDialogCreate), &_cbCallback, 0, 0, 0);
+	}
+}
+
